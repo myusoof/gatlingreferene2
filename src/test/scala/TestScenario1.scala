4427717ca6083a4a75bddab988aedbd3c74bf579
myusoof <Yusoof@123>
2016-10-05 17:53:10 +0530

    performance tesging

--- src/test/scala/TestScenario1.scala
+++ src/test/scala/TestScenario1.scala
@@ -1,13 +1,13 @@
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 import io.gatling.http
-
+import scala.concurrent.duration._
 /**
   * Created by yusoof on 5/10/16.
   */
 class TestScenario1 extends Simulation{
 
-  val scenario1 = scenario("first scenario").exec(http.Predef.http("").get("http://www.google.com"))
+  val scenario1 = scenario("first scenario").exec(http.Predef.http("post call").get("http://jsonplaceholder.typicode.com/posts/1"))
 
-  setUp(scenario1.inject(atOnceUsers(1)))
+  setUp(scenario1.inject(rampUsersPerSec(2) to (10) during(20 seconds))).maxDuration(40 seconds)
 }
